3ea2f0f4601b75d64e6e3d9547a165169c74699c
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-07-29 13:32:42 +0200

    added: admin search action now performs a basic search

--- actions/admin_search.php
+++ actions/admin_search.php
@@ -2,4 +2,44 @@
 
 $q = get_input('q');
 
-echo "you searched for $q";
+/*
+ * $q can be a string or a json formatted query like the following
+ *
+ * {
+ *		"query": {
+ *			"match" : {
+ *				"name" : {
+ *					"query" : "paul"
+ *				}
+ *			}
+ *		}
+ *	}
+ *
+ */
+
+$index = elgg_get_plugin_setting('index', 'elasticsearch');
+
+$client = elasticsearch_get_client();
+
+$searchParams = ['index' => $index];
+
+$json_data = @json_decode($q, true);
+
+if (is_array($json_data)) {
+	$searchParams['body'] = $json_data;
+} else {
+	$searchParams['body'] = [
+		'query' => [
+			'match' => [
+				'_all' => $q
+			]
+		]
+	];
+}
+
+$result = $client->search($searchParams);
+if ($result) {
+	echo var_export($result, true);
+	
+	echo time();
+}
