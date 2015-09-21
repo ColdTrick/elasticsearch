36fd8fded1c86650f25ec867ee8019d46489922d
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-09-21 16:24:51 +0200

    added: user/group tag profile fields get appended to the tags field

--- start.php
+++ start.php
@@ -46,6 +46,7 @@ function elasticsearch_init() {
 	elgg_register_plugin_hook_handler('to:object', 'entity', array('ColdTrick\ElasticSearch\Export', 'entityRelationshipsToObject'));
 	elgg_register_plugin_hook_handler('to:object', 'entity', array('ColdTrick\ElasticSearch\Export', 'entityMetadataToObject'));
 	elgg_register_plugin_hook_handler('to:object', 'entity', array('ColdTrick\ElasticSearch\Export', 'entityCountersToObject'));
+	elgg_register_plugin_hook_handler('to:object', 'entity', array('ColdTrick\ElasticSearch\Export', 'profileTagFieldsToTags'));
 	elgg_register_plugin_hook_handler('export:metadata_names', 'elasticsearch', array('ColdTrick\ElasticSearch\Export', 'exportProfileMetadata'));
 	elgg_register_plugin_hook_handler('export:counters', 'elasticsearch', array('ColdTrick\ElasticSearch\Export', 'exportGroupMemberCount'));
 	elgg_register_plugin_hook_handler('index_entity_type_subtypes', 'elasticsearch', array('ColdTrick\ElasticSearch\Export', 'indexEntityTypeSubtypes'));
