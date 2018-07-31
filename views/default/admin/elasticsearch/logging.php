dab812380104a27fd70db404dd4d51f57089a0df
Jerome Bakker <jeabakker@coldtrick.com>
2018-07-31 14:57:07 +0200

    chore: admin logging

--- views/default/admin/elasticsearch/logging.php
+++ views/default/admin/elasticsearch/logging.php
@@ -1,36 +1,42 @@
 <?php
 
+use Elgg\Project\Paths;
+
 $logging_base_dir = elgg_get_data_path() . 'elasticsearch/';
 $path = get_input('path');
 
 $logging_dir = $logging_base_dir . $path;
 
-echo elgg_view('output/longtext', array('value' => elgg_echo('elasticsearch:logging:description')));
+echo elgg_view('output/longtext', [
+	'value' => elgg_echo('elasticsearch:logging:description'),
+]);
 
 if (!empty($path)) {
-	$path = sanitise_filepath($path);
+	$path = Paths::sanitize($path);
 	
 	$parts = explode('/', trim($path, '/'));
 	
-	$urls = array();
-	$urls[] = elgg_view('output/url', array(
+	$urls = [];
+	$urls[] = elgg_view('output/url', [
 		'text' => elgg_echo('elasticsearch:logging:root'),
 		'href' => "admin/elasticsearch/logging",
 		'is_trusted' => true,
-	));
+	]);
 	
 	$new_path = '';
 	foreach ($parts as $part) {
 		$new_path .= "{$part}/";
 		
-		$urls[] = elgg_view('output/url', array(
+		$urls[] = elgg_view('output/url', [
 			'text' => $part,
-			'href' => "admin/elasticsearch/logging?path={$new_path}",
+			'href' => elgg_http_add_url_query_elements('admin/elasticsearch/logging', [
+				'path' => $new_path,
+			]),
 			'is_trusted' => true,
-		));
+		]);
 	}
 	
-	echo elgg_format_element('div', array('class' => 'mbs'), implode(' > ', $urls));
+	echo elgg_format_element('div', ['class' => 'mbs'], implode(' > ', $urls));
 }
 
 try {
@@ -41,37 +47,40 @@ try {
 	return;
 }
 
-$lis = array();
+$lis = [];
 
 foreach ($fs_it as $file => $info) {
-	
-	$li = '';
 	
 	$basename = $info->getBasename();
 	$path = str_ireplace($logging_base_dir, '', $file);
 	
 	if ($fs_it->isDir()) {
-		$li .= elgg_view_icon('list');
-		$li .= elgg_view('output/url', array(
+		$li = elgg_view('output/url', [
+			'icon' => 'list',
 			'text' => $basename,
-			'href' => "admin/elasticsearch/logging?path={$path}",
+			'href' => elgg_http_add_url_query_elements('admin/elasticsearch/logging', [
+				'path' => $path,
+			]),
 			'is_trusted' => true,
 			'class' => 'mls',
-		));
+		]);
 	} else {
-		$li .= elgg_view('output/url', array(
-			'text' => elgg_view_icon('download') . " {$basename}",
-			'href' => "action/elasticsearch/admin/download_log?path={$path}",
+		$li = elgg_view('output/url', [
+			'icon' => 'download',
+			'title' => elgg_echo('download'),
+			'text' => $basename,
+			'href' => elgg_generate_action_url('elasticsearch/admin/download_log', [
+				'path' => $path,
+			]),
 			'is_trusted' => true,
-			'is_action' => true,
-		));
+		]);
 	}
 	
-	$lis[] = $li;
+	$lis[] = elgg_format_element('li', [], $li);
 }
 
 if (!empty($lis)) {
-	echo elgg_format_element('ul', array(), '<li>' . implode('</li><li>', $lis) . '</li>');
+	echo elgg_format_element('ul', [], implode(PHP_EOL, $lis));
 } else {
 	echo elgg_echo('notfound');
 }
