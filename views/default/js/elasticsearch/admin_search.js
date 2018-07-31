2aab4722d86c9d3ad8535ccd3806c6177b806417
Jerome Bakker <jeabakker@coldtrick.com>
2018-07-31 15:11:00 +0200

    fixed: admin search js

--- views/default/js/elasticsearch/admin_search.js
+++ views/default/js/elasticsearch/admin_search.js
@@ -1,6 +1,6 @@
 define(function(require) {
 	
-	var $ = require('jqeury');
+	var $ = require('jquery');
 	var Ajax = require('elgg/Ajax');
 	
 	$(document).on('submit', 'form.elgg-form-elasticsearch-admin-search', function(event) {
@@ -11,8 +11,8 @@ define(function(require) {
 		ajax.action('elasticsearch/admin_search', {
 			data: ajax.objectify($form),
 			success: function(data) {
-				$('#elasticsearch-admin-search-results > .elgg-body').html(data.output);
+				$('#elasticsearch-admin-search-results > .elgg-body').html(data);
 			}
 		});
-	};
+	});
 });
